dff5f2e8c1137b30d551a86f1c7f424ebba2b3b5
ShashankBiplav <Biplavshashank7@gmail.com>
2020-04-07 17:23:35 +0530

    Navigation Drawer header textView(s) displaying dynamic content!

--- app/src/main/java/com/example/packtalk/activities/HomeActivity.java
+++ app/src/main/java/com/example/packtalk/activities/HomeActivity.java
@@ -10,12 +10,19 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.packtalk.R;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
+import com.parse.ParseUser;
+
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -26,22 +33,35 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     //Navigation Drawer UI components
     private NavigationView navigationView;
-    private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+
+//    private String name, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
+        //calling parseServer for user information access
+        final ParseUser currentUser = ParseUser.getCurrentUser();
 
         navigationView =findViewById(R.id.navigationView);
         drawerLayout = findViewById(R.id.drawerLayout);
 
+        //adding header through the code  and inflating header to set TextView(s) dynamically
+        LinearLayout navHeader=(LinearLayout) LayoutInflater.from(this).inflate(R.layout.header, null);
+        navigationView.addHeaderView(navHeader);
+        TextView headerUsername = (TextView) navHeader.findViewById (R.id.navHeaderUsername);
+        TextView headerEmail = (TextView) navHeader.findViewById (R.id.navHeaderEmail);
+        headerUsername.setText(currentUser.get("username").toString());
+        headerEmail.setText(currentUser.get("email").toString());
+
+
+        //custom toolbar
         tabsHolderToolbar = findViewById(R.id.tabs_Holder_Toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(tabsHolderToolbar);
         tabsHolderToolbar.setTitle(R.string.app_name);
+
 
         //Navigation Drawer Menu
         navigationView.bringToFront();
@@ -49,9 +69,27 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close );
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        //removing tint from the icons in navigation drawer
+        navigationView.setItemIconTintList(null);
+
+        //setting username and email of header in navigation drawer dynamically
+//        if( navigationView != null ){
+//            LinearLayout mParent = ( LinearLayout ) navigationView.getHeaderView( 0 );
+//
+//            if( mParent != null ){
+//                // Set your values to the image and text view by declaring and setting as you need to here.
+//                headerUsername.setText( Objects.requireNonNull(currentUser.get("username")).toString());
+//                headerEmail.setText(Objects.requireNonNull(currentUser.get("email")).toString());
+//            }
+//        }
+
 
         //to make navigation drawer items clickable
+        assert navigationView != null;
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
 
 
         //for displaying fragments
@@ -75,27 +113,47 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    //handling navigation drawer activities
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemUpdateProfile:
+                Intent intent = new Intent(HomeActivity.this, UpdateProfile.class);
+                startActivity(intent);
+                break;
+            case R.id.itemLikedPosts:
+                break;
+            case R.id.itemSavedAnnouncements:
+                break;
+            case R.id.itemSettings:
+                break;
+            case R.id.itemLogout:
+                break;
+            case R.id.itemAboutApp:
+                break;
+            case R.id.itemAboutDeveloper:
+                break;
+        }
         return true;
     }
-    //buttons on action bar
+    //displaying custom buttons on action bar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.app_bar_menu, menu);
         return true;
     }
-
+    // function for app bar -> buttons click
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menuProfile:
-                // User chose the "Settings" item, show the app settings UI...
+            case R.id.menuCreatePost:
+                Intent intent = new Intent(HomeActivity.this, CreatePost.class);
+                startActivity(intent);
                 return true;
 
             case R.id.menuAnnouncements:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
+                Intent intent1 = new Intent(HomeActivity.this, MakeAnnouncement.class);
+                startActivity(intent1);
                 return true;
 
             default:
